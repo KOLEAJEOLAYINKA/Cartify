c17f68cc2ab64a35e2a24d18fea7ea17d962087a
KOLEAJEOLAYINKA <koleajeolayinka@gmail.com>
2023-05-07 00:59:07 +0100

    Add CategoryPage component and route to App

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import CategoryPage from "./pages/CategoryPage";
 
+// app Components for routing pages based on routes   
 const App = () => {
   return (
     <Router>
       <Routes>
         <Route>
-          <Route path="/" element={<div>Test</div>} />
+          <Route path="/" element={<CategoryPage/>} />
         </Route>
       </Routes>
     </Router>
